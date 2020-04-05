ab3d815ff04bac37820fdee31a0637a31b6ac431
Masaru Kubo <mkmkmk04281209@gmail.com>
2020-04-05 14:01:45 +0900

    mail authentication

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,20 +31,28 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+
+  # config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
-  config.action_mailer.perform_caching = false
+  if Rails.application.credentials.gmail.present?
+    mail_address = Rails.application.credentials.gmail[:address]
+    password = Rails.application.credentials.gmail[:password]
+  else
+    mail_address = 'admin@example.com'
+    password = 'password'
+  end
 
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :sendmail
   config.action_mailer.smtp_settings = {
-    :enable_starttls_auto => true,
-    :address => "smtp.gmail.com",
-    :port => 587,
-    :domain => 'smtp.gmail.com',
-    :user_name => "mkmkmk04281209@gmail.com", #gmailアドレス
-    :password => "MMkk0428", #gmailパスワード
-    :authentication => 'login',
+      enable_starttls_auto: true,
+      address: "smtp.gmail.com",
+      port: 587,
+      user_name: mail_address,
+      password: password,
+      authentication: "plain"
   }
 
   # Print deprecation notices to the Rails logger.
