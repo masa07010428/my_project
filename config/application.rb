5ed22671902917f5bf476314c3c714938a26123e
Masaru Kubo <mkmkmk04281209@gmail.com>
2020-04-05 12:48:46 +0900

    config install

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,10 @@ module FireFightingEquipment
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # 日本語化
+    config.i18n.default_locale = :ja
+    # タイムゾーン変更
+    config.time_zone = 'Asia/Tokyo'
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,7 +33,19 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   config.action_mailer.perform_caching = false
+
+  config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => 'smtp.gmail.com',
+    :user_name => "mkmkmk04281209@gmail.com", #gmailアドレス
+    :password => "MMkk0428", #gmailパスワード
+    :authentication => 'login',
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
