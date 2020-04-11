7a73a714ea01ecc855952f75f134fc61dd53e28b
Masaru Kubo <mkmkmk04281209@gmail.com>
2020-04-11 12:20:37 +0900

    modified login

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,7 +36,7 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       :enable_starttls_auto => true,
       :address => Settings.smtp[:address],
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'homes#index'
-  get 'homes/about', to: 'homes#about'
-  get 'homes/introduce', to: 'homes#introduce'
-  get 'homes/search', to: 'homes#search'
+  get '/users/sign_out', to: 'devise/sessions#destroy'
+  root '/homes#index'
+  get '/homes/about', to: 'homes#about'
+  get '/homes/introduce', to: 'homes#introduce'
+  get '/homes/search', to: 'homes#search'
 
+
+  
 end
